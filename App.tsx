a055ce4e1f68d47f56d9f5238adf0ac985e4bfcd
bakroe112 <bakroe112@gmail.com>
2026-04-30 15:35:33 +0700

    SIgn up + Sign in

--- App.tsx
+++ App.tsx
@@ -3,11 +3,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeFeed from "./src/screens/HomeFeed";
 import Welcome from "./src/screens/Welcome";
+import SignIn from "./src/screens/SignIn";
+import SignUp from "./src/screens/SignUp";
 const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
+			<Stack.Screen key={"SignUp"} name={"SignUp"} component={SignUp} />
+			<Stack.Screen key={"SignIn"} name={"SignIn"} component={SignIn} />
 			<Stack.Screen key={"HomeFeed"} name={"HomeFeed"} component={HomeFeed} />
 			<Stack.Screen key={"Welcome"} name={"Welcome"} component={Welcome} />
       </Stack.Navigator>
